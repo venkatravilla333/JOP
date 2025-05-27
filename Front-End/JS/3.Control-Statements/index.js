94b0de225d3f7e0a3d90300740e57a8e5c804bae
venkat ravilla <venkatravilla333@gmail.com>
2025-05-27 15:27:14 +0530

    control statements

---
+++ Front-End/JS/3.Control-Statements/index.js
@@ -0,0 +1,114 @@
+
+
+// var marks = 101
+
+// if (marks < 35) {
+  //   console.log('Fail')
+  // } else if (marks >= 35 && marks < 60) {
+    //   console.log('Second class')
+    // } else if (marks >= 60 && marks < 75) {
+//   console.log('First class')
+// } else if (marks >= 75 && marks <= 100) {
+  //   console.log('Distinction')
+  // } else {
+    //   console.log('Invalid marks')
+// }
+
+// var marks = 65
+
+// switch (marks) {
+//   case marks < 35:
+//     console.log('fail')
+//     break;
+//     case marks >= 35 && marks < 60:
+//     console.log('Second class')
+//     break;
+//   case marks >= 60 && marks < 75:
+//     console.log('First class')
+//     break;
+//   case marks >= 75 && marks <= 100:
+//     console.log('Distinction')
+//     break;
+//   default:
+//     console.log('Invalid marks')
+  
+// }
+
+
+// var weekDay = 4
+
+// if (weekDay === 1) {
+//   console.log('monday')
+// } else if (weekDay === 2) {
+//   console.log('Thuesday')
+// } else if (weekDay === 3) {
+//   console.log('Wedness day')
+// } else if (weekDay === 4) {
+//   console.log('Thursday')
+// } else if (weekDay === 5) {
+//   console.log('Friday')
+// } else if (weekDay === 6) {
+//   console.log('Saturday')
+// } else if (weekDay === 7) {
+//   console.log('Sunday')
+// } else {
+//   console.log('Invalid day')
+// }
+
+// var weekDay = 4
+
+// switch (weekDay) {
+//   case 1:
+//     console.log('monday')
+//     break;
+//   case 2:
+//     console.log('Thuesday')
+//     break;
+//   case 3:
+//     console.log('Wedness day')
+//     break;
+//   case 4:
+//     console.log('Thusday')
+//     break;
+//   case 5:
+//     console.log('Friday')
+//     break;
+//   case 6:
+//     console.log('Saturday')
+//     break;
+//   case 7:
+//     console.log('Sunday')
+//     break;
+//   default:
+//     console.log('Ivalid day')
+// }
+
+
+// var customer = 'Mango'
+
+// if (customer === 'Gold') {
+//   console.log('Gold customer')
+// } else if (customer === 'Silver') {
+//   console.log('Silver customer')
+// } else if (customer === 'Iron') {
+//   console.log('Iron customer')
+// } else {
+//   console.log('Invalid customer')
+// }
+
+
+var customer = 'Gold'
+
+switch (customer) {
+  case 'Gold':
+    console.log('Gold customer')
+    break;
+  case 'Silver':
+    console.log('Silver customer')
+    break;
+  case 'Iron':
+    console.log('Iron customer')
+    break;
+  default:
+    console.log('Unknown customer')
+}
