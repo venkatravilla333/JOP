8d52efa5e365387d5babae38a3d0bfb9e944c055
venkat ravilla <venkatravilla333@gmail.com>
2025-05-27 14:37:06 +0530

    first commit

---
+++ Front-End/JS/1.Basics/index.js
@@ -0,0 +1,68 @@
+
+
+// function fetchData() {
+//   //api
+// }
+
+// var a = 10
+
+// var b = 20
+
+// var res = a + b
+
+// var parent = document.getElementById('parent')
+
+// var btn = document.getElementById('btn')
+// console.log(btn)
+// console.log(parent)
+
+// function getData() {
+//   if (res) {
+//     console.log(res)
+//     for (var i = 0; i <= 100; i++){
+//       var para = document.createElement('p')
+//       console.log(para)
+//       para.innerText = res
+//       parent.appendChild(para)
+  
+//     }
+//   } else {
+//     var para = document.createElement('p')
+//     para.innerText = 'No value'
+//    parent.appendChild(para)
+//   }
+  
+// }
+
+// btn.addEventListener('click', getData)
+
+// variables & data types  & typeof operator 
+
+// var a = 'sachin'
+// console.log(a, typeof a)
+
+// var b = 40
+// console.log(b, typeof b)
+
+// var c = true
+// console.log(c, typeof c)
+
+// var d
+// console.log(d, typeof d)
+
+// var e = null
+// console.log(e, typeof e)
+
+
+// var f = { name: 'sachin' }
+// console.log(f, typeof f)
+
+// var g = [1, 2, 3]
+// console.log(g, typeof g)
+// console.log(Array.isArray(g))
+
+// var h = () => {
+//   var a = 100
+// }
+// console.log(h, typeof h)
+// h()
---
+++ Front-End/JS/2.Operators/index.js
@@ -0,0 +1,99 @@
+
+// var a = 20
+
+// var b = 10
+
+//arithmatic
+
+// console.log( a + b)
+// console.log( a - b)
+// console.log( a * b)
+// console.log( a / b)
+// console.log( a % b)
+// console.log(a ** b)
+
+// console.log(a++)
+// console.log(a)
+// console.log(++a)
+// console.log(a)
+// console.log(--a)
+// console.log(a--)
+// console.log(a++)
+// console.log(a)
+
+//assignment
+
+// var a = 10
+
+//comparision
+
+//ralational
+
+
+// var a = 11
+// var b = 10
+
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a < b)
+// console.log(a <= b)
+
+//equality
+
+// var a = 10
+// var b = 11
+// var b = 10
+// var b = '10'
+// var b = a
+// var b = [10]
+// var a = {value: 10}
+// var b = {value: 10}
+// var b = a
+// var a = undefined
+// var b = null
+
+// var a = [1,2,3]
+// var b = [1,2,3]
+// var b = [10,20,30]
+
+// var a = ()=>{}
+// var b = ()=>{}
+// var b = a
+
+// var a = 10
+
+// var b = ()=>{}
+
+// console.log(a == b)
+// console.log(a === b)
+// console.log(a != b)
+// console.log(a !== b)
+
+
+//ternary operator
+
+// var age = 18
+
+// var res = age >= 18 ? 'Having vote right' : 'No vote right'
+
+// console.log(res)
+
+
+//Logical operators
+
+// var income = true
+
+// var cibil = false
+// var cibil = true
+
+// var localAddress = false
+
+// var RBICC = income && cibil && localAddress
+// var RBLCC = (income || cibil) && localAddress
+
+// console.log(RBICC)
+
+// var income = true
+
+// console.log(income)
+// console.log(!income)
