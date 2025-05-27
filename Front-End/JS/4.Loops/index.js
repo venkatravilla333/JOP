cb9d3a85771fc62f70ab7ef72d2b6378e27aaabe
venkat ravilla <venkatravilla333@gmail.com>
2025-05-27 17:27:48 +0530

    loops

---
+++ Front-End/JS/4.Loops/index.js
@@ -0,0 +1,185 @@
+
+var name = 'sachin'
+
+//without using loops
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+// with using loops
+
+// for (var i = 1; i <= 10; i++){
+//   if(i===5) continue
+//   console.log(i, name)
+// }
+
+// var i = 1
+
+// while (i<=10) {
+//   console.log(i, name)
+//   i++
+// }
+
+// var i = 1
+
+// do {
+//   console.log(i, name)
+//   i++
+// }while(i<=10)
+
+
+// var obj = {}
+// console.log(obj)
+
+// var arr = []
+// console.log(arr)
+
+var person = {
+  name: 'sachin',
+  age: 40,
+  color: 'red',
+  city: 'mumbai',
+  height: 5,
+  play: () => {
+    console.log('Play')
+  }
+}
+
+console.log(person)
+
+// without loop
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.color)
+// console.log(person.city)
+// person.play()
+
+// for (var x in person) {
+//   console.log(x, person[x])
+// }
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+// console.log(arr[6])
+// console.log(arr[7])
+// console.log(arr[8])
+// console.log(arr[9])
+
+// for (var index in arr) {
+//   console.log(index, arr[index])
+// }
+
+for (var value of person) {
+  console.log(value)
+}
+
+
+
+
