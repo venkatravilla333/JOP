60b208d23ed342940bbcd65d04eb156d25fc7b64
venkat ravilla <venkatravilla333@gmail.com>
2025-06-02 15:47:21 +0530

    arrays

--- Front-End/JS/7.Arrays/index.js
+++ Front-End/JS/7.Arrays/index.js
@@ -28,8 +28,8 @@
 //Remove all items from array
 
 
-var arr1 = [1, 2, 3, 4, 5]
-var arr2 = arr1
+// var arr1 = [1, 2, 3, 4, 5]
+// var arr2 = arr1
 
 
 // arr1.length = 0
@@ -190,30 +190,100 @@ var arr = [{course: 'nodejs'}, {course: 'reactjs'}, {course: 'html'}, {course: '
 
 // how to remove deplicate values from array
 
-var arr = [1, 2, 3, 4, 3, 5, 3]
-console.log(arr)
+// var arr = [1, 2, 3, 4, 3, 5, 3]
+// console.log(arr)
 
 // console.log(new Set(arr))
 
 // var uniqueArr = [...new Set(arr)]
 // console.log(uniqueArr)
 
-var uniqueArr = []
+// var uniqueArr = []
 
-function removeDuplicates(arr) {
-  // console.log(arr)
-  for (var value of arr) {
-    // console.log(value)
-    if (uniqueArr.indexOf(value) === -1) {
-      uniqueArr.push(value)
+// function removeDuplicates(arr) {
+//   // console.log(arr)
+//   for (var value of arr) {
+//     // console.log(value)
+//     if (uniqueArr.indexOf(value) === -1) {
+//       uniqueArr.push(value)
+//     }
+
+//   }
+// }
+
+// removeDuplicates(arr)
+
+// console.log(uniqueArr)
+
+// var arr = [1, 2, [10, 20], [100, 200, [1000, 2000]]]
+
+// console.log(arr)
+
+// var flattenArr = arr.flat(Infinity)
+// console.log(flattenArr)
+
+// var flattenArr = []
+
+// function flattenFun(arr) {
+//   console.log(arr)
+//   for (var value of arr) {
+//     console.log(value)
+//     if (Array.isArray(value)) {
+//       flattenFun(value)
+//     } else {
+//       flattenArr.push(value)
+//     }
+//   }
+// }
+
+// flattenFun(arr)
+
+// console.log(flattenArr)
+
+//sorting
+
+var arr = ['sachin', 'kohli', 'dhoni', 'gill']
+// var arr = [3, 8, 4, 1]
+// var arr = [3, 8, 40, 1]
+// var arr = [{course: 'nodejs'}, {course: 'html'}, {course: 'reactjs'}]
+
+
+console.log(arr)
+
+// var ASO = arr.sort()
+// var DSO = arr.reverse()
+// console.log(ASO)
+// console.log(DSO)
+
+// var aso = arr.sort((a,b) => {
+//   return b-a
+// })
+
+// console.log(aso)
+
+// var aso = arr.sort((a,b) => {
+//   if (a.course > b.course) {
+//     return -1
+//   }
+//   if (a.course < b.course) {
+//     return 1
+//   }
+// })
+
+// console.log(aso)
+
+for (var i = 0; i < arr.length; i++){
+  for (var j = i + 1; j < arr.length; j++){
+    if (arr[i] > arr[j]) {
+      var temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
     }
-
   }
 }
 
-removeDuplicates(arr)
+console.log(arr)
 
-console.log(uniqueArr)
 
 
 
